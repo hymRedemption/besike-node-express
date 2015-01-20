bb16d629c9be4167d2adf0e5c004f8eec69d36a8
he.redemption <he.redemption@gmail.com>
2015-01-20 16:48:57 +0800

    rewrite 9 class

--- index.js
+++ index.js
@@ -1,34 +1,63 @@
 module.exports = myexpress;
 
 var http = require("http");
-function app(req, res){
-    var i = -1;
-    function next(err) {
-        i++;
-        if (app.stack[i] == undefined) {
-            res.statusCode = 404;
-            res.end();
-        } else {
-        //    console.log(i);
-            (app.stack[i])(req, res, next);
-        }
-    }
-    next();
-}
-
-app.stack = [];
-app.use = function (middleware){
-    app.stack.push(middleware);
-};
-
-
-app.listen = function (port, callback) {
-    console.log("listen");
-    var serv = http.createServer(app);
-    return serv.listen(port, callback);
-};
 
 function myexpress(){
+
+    function app(req, res, parientNext){
+        var i = -1;
+
+        function next(err) {
+            i++;
+            var midware = app.stack[i];
+
+            if (midware == undefined) {
+                if (parientNext !== undefined) {
+                    parientNext(err);
+                } else {
+                    res.statusCode = err ? 500 : 404;
+                    res.end();
+                }
+            } else {
+                var varLength = app.stack[i].length;
+
+                if (midware.constructor == myexpress) { 
+                    midware(req, res, next);
+                } else {
+                    if (err) {
+                        (varLength == 4) ? midware(err, req, res, next) : next(err);
+                    } else {
+                        (varLength == 4) ? next() : midware(req, res, next);
+                    }
+                }
+            }
+        }
+
+        try {
+            next();
+        } catch(ex) {
+            if (parientNext !== undefined){
+                parientNext(ex);
+            } else {
+                res.statusCode = 500;
+                res.end();
+            }
+        }
+    }
+
+    app.stack = [];
+    app.constructor = myexpress;
+    app.use = function (middleware){
+        app.stack.push(middleware);
+    };
+
+
+    app.listen = function (port, callback) {
+        console.log("listen");
+        var serv = http.createServer(app);
+        return serv.listen(port, callback);
+    };
+
     return app;
 }
 
